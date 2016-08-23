427b5b78026d5bd281bda6af83410bfb81499c51
Diabloxx <diabloxx50@gmail.com>
2016-08-23 10:05:03 +0200

    Changed to ripple

---
+++ config.example.js
@@ -0,0 +1,11 @@
+//----- Config
+module.exports = {
+	token: "bot_account_login_token",
+	osu_api: "osu_api_key",
+	osu_irc_username: "ripple_irc_user", //optional
+	osu_irc_password: "ripple_irc_password", //optional
+	
+	superadmins: [
+		"discord_user_id"
+	]
+};
